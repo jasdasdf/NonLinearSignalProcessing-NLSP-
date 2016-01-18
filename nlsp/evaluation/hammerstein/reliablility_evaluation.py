74d63c4d22f945f7ea1e31559bd69d1b3bbabc7c
Logesh <logeshthamil@gmail.com>
2016-01-18 10:23:03 +0100

    relaiability evaluation- downsampling model included

--- nlsp/evaluation/hammerstein/reliablility_evaluation.py
+++ nlsp/evaluation/hammerstein/reliablility_evaluation.py
@@ -6,7 +6,7 @@ sweep_samplingrate = 48000
 sweep_length = 2**18
 plot = True
 
-def linearity_evaluation_upsamplingvslpass(sweep_samplingrate, sweep_length):
+def reliabilityofaliasing(sweep_samplingrate, sweep_length):
     """
     Evaluation of the model for linearity.
     The polynomial block power is set to one, so it produces only linear output. But aliasing compensation is done
@@ -21,29 +21,49 @@ def linearity_evaluation_upsamplingvslpass(sweep_samplingrate, sweep_length):
     ip_sweep_spec = sumpf.modules.FourierTransform(ip_sweep_signal)
     UPHModel = nlsp.AliasCompensatingHammersteinModelUpandDown(input_signal=ip_sweep_signal.GetSignal(),
                                                              nonlin_func=nlsp.NonlinearFunction.power_series(1))
-    UPHModel.SetMaximumHarmonic(2)
-    print UPHModel.GetMaximumHarmonic()
     LPHModel = nlsp.AliasCompensatingHammersteinModelLowpass(input_signal=ip_sweep_signal.GetSignal(),
                                                              nonlin_func=nlsp.NonlinearFunction.power_series(1),
                                                              filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
                                                              filterorder=100)
-    LPHModel.SetMaximumHarmonic(5)
+    DOWNHModel = nlsp.AliasCompensatingHammersteinModelDownandUp(input_signal=ip_sweep_signal.GetSignal(),
+                                                                 nonlin_func=nlsp.NonlinearFunction.power_series(1))
     print LPHModel.GetMaximumHarmonic()
+    print UPHModel.GetMaximumHarmonic()
+    print DOWNHModel.GetMaximumHarmonic()
+    UPHModel.SetMaximumHarmonic(2)
+    LPHModel.SetMaximumHarmonic(2)
+    DOWNHModel.SetMaximumHarmonic(2)
+    print LPHModel.GetMaximumHarmonic()
+    print UPHModel.GetMaximumHarmonic()
+    print DOWNHModel.GetMaximumHarmonic()
 
     if plot is True:
+        # down sampling model plot - signal
+        common.plot.plot(ip_sweep_signal.GetSignal(), show=False)
+        common.plot.plot(DOWNHModel.GetNLOutput(), show=False)
+        common.plot.plot(DOWNHModel.GetOutput(), show=True)
+        # up sampling model plot - signal
         common.plot.plot(ip_sweep_signal.GetSignal(), show=False)
         common.plot.plot(UPHModel.GetNLOutput(), show=False)
         common.plot.plot(UPHModel.GetOutput(), show=True)
+        # lowpass model plot - signal
         common.plot.plot(ip_sweep_signal.GetSignal(), show=False)
         common.plot.plot(LPHModel.GetNLOutput(), show=False)
         common.plot.plot(LPHModel.GetOutput(), show=True)
+
         common.plot.log()
+        # down sampling model plot - spectrum
         common.plot.plot(sumpf.modules.FourierTransform(signal=ip_sweep_signal.GetSignal()).GetSpectrum(), show=False)
-        common.plot.plot(sumpf.modules.FourierTransform(signal=LPHModel.GetNLOutput()).GetSpectrum(), show=False)
-        common.plot.plot(sumpf.modules.FourierTransform(signal=LPHModel.GetOutput()).GetSpectrum(), show=True)
+        common.plot.plot(sumpf.modules.FourierTransform(signal=DOWNHModel.GetNLOutput()).GetSpectrum(), show=False)
+        common.plot.plot(sumpf.modules.FourierTransform(signal=DOWNHModel.GetOutput()).GetSpectrum(), show=True)
+        # up sampling model plot - spectrum
         common.plot.plot(sumpf.modules.FourierTransform(signal=ip_sweep_signal.GetSignal()).GetSpectrum(), show=False)
         common.plot.plot(sumpf.modules.FourierTransform(signal=UPHModel.GetNLOutput()).GetSpectrum(), show=False)
         common.plot.plot(sumpf.modules.FourierTransform(signal=UPHModel.GetOutput()).GetSpectrum(), show=True)
+        # lowpass model plot - spectrum
+        common.plot.plot(sumpf.modules.FourierTransform(signal=ip_sweep_signal.GetSignal()).GetSpectrum(), show=False)
+        common.plot.plot(sumpf.modules.FourierTransform(signal=LPHModel.GetNLOutput()).GetSpectrum(), show=False)
+        common.plot.plot(sumpf.modules.FourierTransform(signal=LPHModel.GetOutput()).GetSpectrum(), show=True)
 
 
-linearity_evaluation_upsamplingvslpass(sweep_samplingrate, sweep_length)
+reliabilityofaliasing(sweep_samplingrate, sweep_length)

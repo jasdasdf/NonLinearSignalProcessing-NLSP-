106e8dbae9a6ccd6ba4c6e72e9a0506b204ae323
Logesh <logeshthamil@gmail.com>
2016-01-21 10:30:25 +0100

    find freq, magnitude optional argument added

--- nlsp/tests/_common/find_frequencies.py
+++ nlsp/tests/_common/find_frequencies.py
@@ -2,10 +2,11 @@ import sumpf
 import nlsp
 import common
 
-def find_frequencies(input):
+def find_frequencies(input,magnitude=100):
     """
     finds the frequencies in the input signal or spectrum
     :param input: the input signal or spectrum whose frequencies has to be found
+    :param magnitude: the magnitude above which the frequency has to be detected
     :return: list of frequencies which are found in the input signal
     """
     if isinstance(input,(sumpf.Signal)):
@@ -16,7 +17,7 @@ def find_frequencies(input):
     for c in ip.GetChannels():
         channel_h = []
         for i, s in enumerate(c):
-            if abs(s)**2 > 100:
+            if abs(s)**2 > magnitude:
                 channel_h.append(i)
         frequencies.append(channel_h)
     h = [item for sublist in frequencies for item in sublist]

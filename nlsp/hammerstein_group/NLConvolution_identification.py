c090e5fab32e3b9abc2d0efaf47d9c4094a274ae
Logesh <logeshthamil@gmail.com>
2016-01-22 16:55:08 +0100

    nonlinear convolution type of system identification, need to be optimized, produces reliable result

--- nlsp/hammerstein_group/NLConvolution_identification.py
+++ nlsp/hammerstein_group/NLConvolution_identification.py
@@ -21,46 +21,57 @@ def nonlinearconvolution_identification_filter(input_sweep, output_sweep):
     :param output_sweep: the output signal which is observed from the nonlinear system
     :return: the impulse response of the filters of hammerstein group model
     """
-    if isinstance(input_sweep ,(sumpf.Signal)):
-        ip_s = input_sweep
-        ip = sumpf.modules.FourierTransform(signal=input_sweep).GetSpectrum()
-    else:
-        ip_s = sumpf.modules.InverseFourierTransform(spectrum=input_sweep).GetSignal()
-        ip = input_sweep
-    if isinstance(output_sweep ,(sumpf.Signal)):
-        op = sumpf.modules.FourierTransform(signal=output_sweep).GetSpectrum()
-    else:
-        op = output_sweep
-    spec_inversion = sumpf.modules.RegularizedSpectrumInversion(spectrum=ip).GetOutput()
-    mul = sumpf.modules.MultiplySpectrums(spectrum1=spec_inversion, spectrum2=op).GetOutput()
-    it = sumpf.modules.InverseFourierTransform(spectrum=mul).GetSignal()
-    direct_ir = sumpf.modules.CutSignal(signal=it,start=0,stop=2**12).GetOutput()
-    merger = sumpf.modules.MergeSignals(on_length_conflict=sumpf.modules.MergeSignals.FILL_WITH_ZEROS)
-    merger.AddInput(direct_ir)
-    for i in range(4):
-        split_harm = sumpf.modules.FindHarmonicImpulseResponse(impulse_response=it,
-                                                               harmonic_order=i+2).GetHarmonicImpulseResponse()
-        resampler = sumpf.modules.ResampleSignal(signal=split_harm, samplingrate=ip_s.GetSamplingRate()).GetOutput()
-        merger.AddInput(resampler)
-    harm_spec = sumpf.modules.FourierTransform(signal=merger.GetOutput()).GetSpectrum()
-    harmonics = []
-    for i in range(len(harm_spec.GetChannels())):
-        split =  sumpf.modules.SplitSpectrum(data=harm_spec,channels=[i]).GetOutput()
-        harmonics.append(split)
-    H = []
-    H.append(harmonics[0] + 3*harmonics[2] +5*harmonics[4])
-    H.append(sumpf.modules.AmplifySpectrum(input=harmonics[1],factor=2j).GetOutput() +
-             sumpf.modules.AmplifySpectrum(input=harmonics[3],factor=8j).GetOutput())
-    H.append(-4*harmonics[2] - 20*harmonics[4])
-    H.append(sumpf.modules.AmplifySpectrum(input=harmonics[3],factor=-8j).GetOutput())
-    H.append(16*harmonics[4])
-    h = []
-    for kernel in H:
-        ift = sumpf.modules.InverseFourierTransform(spectrum=kernel).GetSignal()
-        h.append(ift)
-    return h
+    sweep_start_freq = 20.0
+    sweep_stop_freq = 20000.0
+    sweep_length = 2**15
+    branch = 5
 
-def nonlinearconvolution_identification_nlfunction():
+    if isinstance(input_sweep ,(sumpf.Signal)):
+        ip_signal = input_sweep
+        ip_spectrum = sumpf.modules.FourierTransform(signal=input_sweep).GetSpectrum()
+    else:
+        ip_signal = sumpf.modules.InverseFourierTransform(spectrum=input_sweep).GetSignal()
+        ip_spectrum = input_sweep
+    if isinstance(output_sweep ,(sumpf.Signal)):
+        op_spectrum = sumpf.modules.FourierTransform(signal=output_sweep).GetSpectrum()
+    else:
+        op_spectrum = output_sweep
+    inversed_ip = sumpf.modules.RegularizedSpectrumInversion(spectrum=ip_spectrum,start_frequency=sweep_start_freq,
+                                                             stop_frequency=sweep_stop_freq).GetOutput()
+    tf_sweep = sumpf.modules.MultiplySpectrums(spectrum1=inversed_ip, spectrum2=op_spectrum).GetOutput()
+    ir_sweep = sumpf.modules.InverseFourierTransform(spectrum=tf_sweep).GetSignal()
+    ir_sweep_direct = sumpf.modules.CutSignal(signal=ir_sweep,start=0,stop=2**8).GetOutput()
+    ir_merger = sumpf.modules.MergeSignals(on_length_conflict=sumpf.modules.MergeSignals.FILL_WITH_ZEROS)
+    ir_merger.AddInput(ir_sweep_direct)
+
+    for i in range(branch-1):
+        split_harm = sumpf.modules.FindHarmonicImpulseResponse(impulse_response=ir_sweep,
+                                                               harmonic_order=i+2,
+                                                               sweep_start_frequency=sweep_start_freq,
+                                                               sweep_stop_frequency=sweep_stop_freq,
+                                                               sweep_duration=sweep_length/
+                                                               ip_signal.GetSamplingRate()).GetHarmonicImpulseResponse()
+        ir_merger.AddInput(sumpf.Signal(channels=split_harm.GetChannels(),
+                                        samplingrate=ip_signal.GetSamplingRate(), labels=split_harm.GetLabels()))
+    tf_harmonics_all = sumpf.modules.FourierTransform(signal=ir_merger.GetOutput()).GetSpectrum()
+    harmonics_tf = []
+    for i in range(len(tf_harmonics_all.GetChannels())):
+        tf_harmonics =  sumpf.modules.SplitSpectrum(data=tf_harmonics_all, channels=[i]).GetOutput()
+        harmonics_tf.append(tf_harmonics)
+    Volterra_tf = []
+    Volterra_tf.append(harmonics_tf[0] + (3)*harmonics_tf[2] +(5)*harmonics_tf[4])
+    Volterra_tf.append(sumpf.modules.AmplifySpectrum(input=harmonics_tf[1],factor=2j).GetOutput() +
+             sumpf.modules.AmplifySpectrum(input=harmonics_tf[3],factor=8j).GetOutput())
+    Volterra_tf.append(-4*harmonics_tf[2] - 20*harmonics_tf[4])
+    Volterra_tf.append(sumpf.modules.AmplifySpectrum(input=harmonics_tf[3],factor=-8j).GetOutput())
+    Volterra_tf.append(16*harmonics_tf[4])
+    Volterra_ir = []
+    for kernel in Volterra_tf:
+        ift = sumpf.modules.InverseFourierTransform(spectrum=kernel).GetSignal()
+        Volterra_ir.append(ift)
+    return Volterra_ir
+
+def nonlinearconvolution_identification_nlfunction(branches):
     """
     This function returns the nonlinear function to the nonlinear blocks of the hammerstein group model.
     In nonlinear convolution method the nonlinear function is defined by power series expansion, Hence it returns
@@ -68,7 +79,7 @@ def nonlinearconvolution_identification_nlfunction():
     The power series expansion is done by using nlsp classes.
     :return: the nonlinear functions to the hammerstein group model
     """
-    nl = []
-    for i in range(5):
-        nl.append(nlsp.function_factory.power_series(i+1))
-    return nl
+    nl_functions = []
+    for i in range(branches):
+        nl_functions.append(nlsp.function_factory.power_series(i+1))
+    return nl_functions

115be30fae7bddc354de8ba08ce1898e5b070f74
Logesh <logeshthamil@gmail.com>
2016-02-01 12:26:03 +0100

    function to generate logarithmically seperated bandpass filter

--- nlsp/common/helper.py
+++ nlsp/common/helper.py
@@ -80,3 +80,36 @@ def get_sweep_harmonics_ir(excitation, response, sweep_start_freq, sweep_stop_fr
         merger.AddInput(harmonics)
     return merger.GetOutput()
 
+def log_bpfilter(start_freq,stop_freq,branches,input):
+    """
+    Generates logarithmically seperated band pass filters between start and stop frequencies.
+    :param start_freq: the start frequency of the bandpass filter
+    :param stop_freq: the stop frequency of the bandpass filter
+    :param branches: the number of branches of bandpass filter
+    :param input: the input signal to get the filter parameters
+    :return:
+    """
+    ip_prp = sumpf.modules.ChannelDataProperties()
+    ip_prp.SetSignal(input)
+    dummy = 10
+    while True:
+        dummy = dummy - 0.1
+        low = 100 * (dummy**1)
+        high = 100 * (dummy**branches)
+        if low > start_freq*2 and high < stop_freq:
+            break
+    frequencies = []
+    for i in range(1,branches+1):
+        frequencies.append(100 * (dummy**i))
+    filter_spec = []
+    for freq in frequencies:
+        spec =  (sumpf.modules.FilterGenerator(filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
+                                            frequency=freq,
+                                            resolution=ip_prp.GetResolution(),
+                                            length=ip_prp.GetSpectrumLength()).GetSpectrum())*\
+                (sumpf.modules.FilterGenerator(filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
+                                            frequency=freq/2,transform=True,
+                                            resolution=ip_prp.GetResolution(),
+                                            length=ip_prp.GetSpectrumLength()).GetSpectrum())
+        filter_spec.append(sumpf.modules.InverseFourierTransform(spec).GetSignal())
+    return filter_spec, frequencies

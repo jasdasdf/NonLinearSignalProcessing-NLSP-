e4f55cabe5f156fcca5443126369abd96b84fc64
Logesh <logeshthamil@gmail.com>
2016-02-02 10:40:31 +0100

    general script to find snr and mse

--- nlsp/evaluation/hammerstein_group/hgm_aliasing_evaluation.py
+++ nlsp/evaluation/hammerstein_group/hgm_aliasing_evaluation.py
@@ -3,6 +3,12 @@ import nlsp
 import common.plot as plot
 
 def sweep_evaluation():
+    """
+    Evaluation of Hammerstein group models using sweep
+    Nonlinear system: hgm with same branch and same nl and linear functions
+    input: sweep of certain length
+    the SNR and MSE between the expected and identified output is evaluted
+    """
     nlsystem_up = nlsp.HammersteinGroupModel_up(nonlinear_functions=nonlinear_functions,
                                               filter_irs=filter_spec_tofind,
                                               max_harmonics=max_harmonics)
@@ -13,6 +19,76 @@ def sweep_evaluation():
                                               attenuation=0.01)
     nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
                                               filter_irs=filter_spec_tofind)
+    nlsystem.SetInput(input_sweep)
+    nlsystem_up.SetInput(input_sweep)
+    nlsystem_lp.SetInput(input_sweep)
+    found_filter_spec_up = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
+                                                                                                     sweep_stop_freq,
+                                                                                                     branches])
+    hgm_up = nlsp.HammersteinGroupModel_up(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=found_filter_spec_up,
+                                              max_harmonics=max_harmonics)
+    hgm_up.SetInput(input_sweep)
+    found_filter_spec_lp = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
+                                                                                                     sweep_stop_freq,
+                                                                                                     branches])
+    hgm_lp = nlsp.HammersteinGroupModel_lp(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=found_filter_spec_lp,
+                                              max_harmonics=max_harmonics,
+                                              filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
+                                              attenuation=0.01)
+    hgm_lp.SetInput(input_sweep)
+    found_filter_spec = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
+                                                                                                     sweep_stop_freq,
+                                                                                                     branches])
+    hgm = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=found_filter_spec)
+    hgm.SetInput(input_sweep)
+    # plot.log()
+    # plot.plot(sumpf.modules.FourierTransform(hgm.GetOutput()).GetSpectrum(), show=False)
+    # plot.plot(sumpf.modules.FourierTransform(nlsystem.GetOutput()).GetSpectrum(), show=True)
+    print "SNR of nl model: %r and identified system: %r and btw nl and iden system,simple: %r" \
+          %(nlsp.get_snr(input_sweep,nlsystem.GetOutput()),
+            nlsp.get_snr(input_sweep,hgm.GetOutput()),
+            nlsp.get_snr(nlsystem.GetOutput(),hgm.GetOutput()))
+    print "MSE of nl model: %r and identified system: %r and btw nl and iden system,simple: %r" \
+          %(nlsp.mean_squared_error(input_sweep,nlsystem.GetOutput()),
+            nlsp.mean_squared_error(input_sweep,hgm.GetOutput()),
+            nlsp.mean_squared_error(nlsystem.GetOutput(),hgm.GetOutput()))
+    # plot.log()
+    # plot.plot(sumpf.modules.FourierTransform(hgm_up.GetOutput()).GetSpectrum(), show=False)
+    # plot.plot(sumpf.modules.FourierTransform(nlsystem_up.GetOutput()).GetSpectrum(), show=True)
+    print "SNR of nl model: %r and identified system: %r and btw nl and iden system,upsample: %r" \
+          %(nlsp.get_snr(input_sweep,nlsystem_up.GetOutput()),
+            nlsp.get_snr(input_sweep,hgm_up.GetOutput()),
+            nlsp.get_snr(nlsystem_up.GetOutput(),hgm_up.GetOutput()))
+    print "MSE of nl model: %r and identified system: %r and btw nl and iden system,upsample: %r" \
+          %(nlsp.mean_squared_error(input_sweep,nlsystem_up.GetOutput()),
+            nlsp.mean_squared_error(input_sweep,hgm_up.GetOutput()),
+            nlsp.mean_squared_error(nlsystem_up.GetOutput(),hgm_up.GetOutput()))
+    # plot.log()
+    # plot.plot(sumpf.modules.FourierTransform(hgm_lp.GetOutput()).GetSpectrum(), show=False)
+    # plot.plot(sumpf.modules.FourierTransform(nlsystem_lp.GetOutput()).GetSpectrum(), show=True)
+    print "SNR of nl model: %r and identified system: %r and btw nl and iden system,lowpass: %r" \
+          %(nlsp.get_snr(input_sweep,nlsystem_lp.GetOutput()),
+            nlsp.get_snr(input_sweep,hgm_lp.GetOutput()),
+            nlsp.get_snr(nlsystem_lp.GetOutput(),hgm_lp.GetOutput()))
+    print "MSE of nl model: %r and identified system: %r and btw nl and iden system,lowpass: %r" \
+          %(nlsp.mean_squared_error(input_sweep,nlsystem_lp.GetOutput()),
+            nlsp.mean_squared_error(input_sweep,hgm_lp.GetOutput()),
+            nlsp.mean_squared_error(nlsystem_lp.GetOutput(),hgm_lp.GetOutput()))
+
+def harmonics_evaluation():
+    nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=filter_spec_tofind)
+    nlsystem_up = nlsp.HammersteinGroupModel_up(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=filter_spec_tofind,
+                                              max_harmonics=max_harmonics)
+    nlsystem_lp = nlsp.HammersteinGroupModel_lp(nonlinear_functions=nonlinear_functions,
+                                              filter_irs=filter_spec_tofind,
+                                              max_harmonics=max_harmonics,
+                                              filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
+                                              attenuation=0.01)
     nlsystem.SetInput(input_sweep)
     nlsystem_up.SetInput(input_sweep)
     nlsystem_lp.SetInput(input_sweep)
@@ -39,61 +115,6 @@ def sweep_evaluation():
                                               filter_irs=found_filter_spec)
     hgm.SetInput(input_sweep)
     plot.log()
-    plot.plot(sumpf.modules.FourierTransform(hgm.GetOutput()).GetSpectrum(), show=False)
-    plot.plot(sumpf.modules.FourierTransform(nlsystem.GetOutput()).GetSpectrum(), show=True)
-    print "Signal to noise ratio of ip: %r and op: %r of chebyshev series nl convolution method normal" \
-          %(nlsp.get_snr(input_sweep,nlsystem.GetOutput()),
-            nlsp.get_snr(input_sweep,hgm.GetOutput()))
-    plot.log()
-    plot.plot(sumpf.modules.FourierTransform(hgm_up.GetOutput()).GetSpectrum(), show=False)
-    plot.plot(sumpf.modules.FourierTransform(nlsystem_up.GetOutput()).GetSpectrum(), show=True)
-    print "Signal to noise ratio of ip: %r and op: %r of chebyshev series nl convolution method up" \
-          %(nlsp.get_snr(input_sweep,nlsystem_up.GetOutput()),
-            nlsp.get_snr(input_sweep,hgm_up.GetOutput()))
-    plot.log()
-    plot.plot(sumpf.modules.FourierTransform(hgm_lp.GetOutput()).GetSpectrum(), show=False)
-    plot.plot(sumpf.modules.FourierTransform(nlsystem_lp.GetOutput()).GetSpectrum(), show=True)
-    print "Signal to noise ratio of ip: %r and op: %r of chebyshev series nl convolution method lp" \
-          %(nlsp.get_snr(input_sweep,nlsystem_lp.GetOutput()),
-            nlsp.get_snr(input_sweep,hgm_lp.GetOutput()))
-
-def harmonics_evaluation():
-    nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=filter_spec_tofind)
-    nlsystem_up = nlsp.HammersteinGroupModel_up(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=filter_spec_tofind,
-                                              max_harmonics=max_harmonics)
-    nlsystem_lp = nlsp.HammersteinGroupModel_lp(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=filter_spec_tofind,
-                                              max_harmonics=max_harmonics,
-                                              filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
-                                              attenuation=0.01)
-    nlsystem.SetInput(input_sweep)
-    nlsystem_up.SetInput(input_sweep)
-    nlsystem_lp.SetInput(input_sweep)
-    found_filter_spec_up = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
-                                                                                                     sweep_stop_freq,
-                                                                                                     branches])
-    hgm_up = nlsp.HammersteinGroupModel_up(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=found_filter_spec_up,
-                                              max_harmonics=max_harmonics)
-    hgm_up.SetInput(input_sweep)
-    found_filter_spec_lp = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
-                                                                                                     sweep_stop_freq,
-                                                                                                     branches])
-    hgm_lp = nlsp.HammersteinGroupModel_lp(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=found_filter_spec_lp,
-                                              max_harmonics=max_harmonics,
-                                              filterfunction=sumpf.modules.FilterGenerator.BUTTERWORTH(order=100),
-                                              attenuation=0.01)
-    hgm_lp.SetInput(input_sweep)
-    found_filter_spec = nlsp.nonlinearconvolution_powerseries_filter(input_sweep,nlsystem.GetOutput(),[sweep_start_freq,
-                                                                                                     sweep_stop_freq,
-                                                                                                     branches])
-    hgm = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
-                                              filter_irs=found_filter_spec)
-    hgm.SetInput(input_sweep)
-    plot.log()
     print "Simple hammerstein model harmonics plot"
     plot.plot(nlsp.get_sweep_harmonics_spectrum(input_sweep,nlsystem.GetOutput(),sweep_start_freq,sweep_stop_freq,branches),show=False)
     plot.plot(nlsp.get_sweep_harmonics_spectrum(input_sweep,hgm.GetOutput(),sweep_start_freq,sweep_stop_freq,branches))
@@ -113,10 +134,13 @@ input_sweep = sumpf.modules.SweepGenerator(samplingrate=sampling_rate,length=swe
                                                start_frequency=sweep_start_freq,
                                                stop_frequency=sweep_stop_freq).GetSignal()
 nonlinear_functions = nlsp.nonlinearconvolution_powerseries_nlfunction(branches)
+#nonlinear_functions = nlsp.nonlinearconvolution_chebyshev_nlfunction(branches)
 #nonlinear_functions = [nlsp.function_factory.power_series(1),]*branches
-filter_spec_tofind,frequencies = nlsp.log_bpfilter(sweep_start_freq,sweep_stop_freq,branches,input_sweep)
-#filter_spec_tofind = [sumpf.modules.ImpulseGenerator(samplingrate=input_sweep.GetSamplingRate(),length=len(input_sweep)).GetSignal(),]*branches
+#nonlinear_functions = [nlsp.function_factory.chebyshev1_polynomial(1),]*branches
+#filter_spec_tofind = nlsp.log_bpfilter(sweep_start_freq,sweep_stop_freq,branches,input_sweep)
+filter_spec_tofind = [sumpf.modules.ImpulseGenerator(samplingrate=input_sweep.GetSamplingRate(),length=len(input_sweep)).GetSignal(),]*branches
 max_harmonics = [1,2,3,4,5]
+#max_harmonics = [1,]*branches
 
 sweep_evaluation()
-harmonics_evaluation()
+# harmonics_evaluation()

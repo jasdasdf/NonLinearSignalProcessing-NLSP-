6f7388814513e04b7fc146dae4d6481e42541e46
Logesh <logeshthamil@gmail.com>
2016-02-11 12:04:33 +0100

    documentation of hgm_series

--- nlsp/hammerstein_group/hammerstein_group_series.py
+++ nlsp/hammerstein_group/hammerstein_group_series.py
@@ -1,12 +1,21 @@
 import sumpf
 
 class HammersteinGroup_Series(object):
-
+    """
+    Class to connect Hammerstein Group Models in series
+    """
     def __init__(self, input_signal=None,
                  nonlinear_functions=None,
                  filter_irs=None,
                  max_harmonics=None,
                  hgm_type=None):
+        """
+        :param input_signal: the input signal
+        :param nonlinear_functions: the tuple of nonlinear functions to hgm
+        :param filter_irs: the tuple of filter impulse response to hgm
+        :param max_harmonics: the tuple of max harmonics to hgm
+        :param hgm_type: the type of hammerstein group model, eg. nlsp.HammersteinGroupModel_up
+        """
         if input_signal is None:
             self.__signal = sumpf.Signal()
         else:
@@ -33,4 +42,9 @@ class HammersteinGroup_Series(object):
             self.inputstage.SetInput(h.GetOutput())
 
     def GetOutput(self,segment):
+        """
+        Get output method of the hammerstein group model connected in series
+        :param segment: the segment of hgm
+        :return: the output of corresponding hgm
+        """
         return self.hmodels[segment-1].GetOutput()

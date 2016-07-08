4d8ae9499b353290c74fe7b042ac0be2541682c9
Logesh <logeshthamil@gmail.com>
2016-07-08 13:42:31 +0200

    simple nonlinear acoustic echo canceller

--- nlsp/nlaec/simple_nlaec.py
+++ nlsp/nlaec/simple_nlaec.py
@@ -3,17 +3,20 @@ import nlsp
 import nlsp.common.plots as plot
 
 sampling_rate = 48000
-length = 2**19
+length = 2**18
 start_freq = 20.0
 stop_freq = 20000.0
+echo_decay = 0.1
+branches = 5
+shift = 800
 
 normal = sumpf.modules.NoiseGenerator.GaussianDistribution(mean=0.0,standard_deviation=1.5)
 uniform = sumpf.modules.NoiseGenerator.UniformDistribution()
 pink = sumpf.modules.NoiseGenerator.PinkNoise()
 laplace = sumpf.modules.NoiseGenerator.LaplaceDistribution(mean=0.0,scale=0.3)
 
-sine = sumpf.modules.SweepGenerator(length=length, start_frequency=start_freq, stop_frequency=stop_freq,
-                                    samplingrate=sampling_rate)
+sine = nlsp.NovakSweepGenerator_Sine(sampling_rate=sampling_rate, length=length, start_frequency=start_freq,
+                                   stop_frequency=stop_freq)
 cos = nlsp.NovakSweepGenerator_Cosine(sampling_rate=sampling_rate, length=length, start_frequency=start_freq,
                                    stop_frequency=stop_freq)
 wgn_normal = nlsp.WhiteGaussianGenerator(sampling_rate=sampling_rate, length=length, start_frequency=start_freq,
@@ -25,48 +28,40 @@ wgn_pink = nlsp.WhiteGaussianGenerator(sampling_rate=sampling_rate, length=lengt
 wgn_laplace = nlsp.WhiteGaussianGenerator(sampling_rate=sampling_rate, length=length, start_frequency=start_freq,
                                    stop_frequency=stop_freq, distribution=laplace)
 nl_system = "C:/Users/diplomand.8/Desktop/evaluation/filter_kernel/LS_nonlinearconvolution_powerseries_temporalreversal_3_261948.npz"
+nl_system_iden_excitation = sine
+system_identification_alg = nlsp.nonlinearconvolution_powerseries_temporalreversal
+speech = wgn_pink.GetOutput()
 
-def generate_nlecho(input):
-    # d1 = sumpf.modules.ImpulseGenerator(delay=0,samplingrate=sampling_rate,length=length).GetSignal()
-    # d2 = sumpf.modules.ImpulseGenerator(delay=0.2,samplingrate=sampling_rate,length=length).GetSignal()
-    # d = sumpf.modules.AddSignals(signal1=d1, signal2=d2).GetOutput()
-    # hgm = nlsp.construct_hgm(kernelfile=nl_system)
-    # hgm.SetInput(input)
-    # signal = sumpf.modules.FourierTransform(hgm.GetOutput()).GetSpectrum()
-    # echo = sumpf.modules.FourierTransform(d).GetSpectrum()
-    # signalandecho = sumpf.modules.MultiplySpectrums(signal,echo).GetOutput()
-    # signalandecho = sumpf.modules.InverseFourierTransform(signalandecho).GetSignal()
-
-
-    #alternative method
+def echoic_nonlinear_chamber(input):
     hgm = nlsp.construct_hgm(kernelfile=nl_system)
     hgm.SetInput(input)
-    s1 = sumpf.modules.ShiftSignal(signal=hgm.GetOutput(),shift=10,circular=False).GetOutput()
-    s2 = sumpf.modules.ShiftSignal(signal=hgm.GetOutput(),shift=30,circular=False).GetOutput()
-    s = s1 + s2 + hgm.GetOutput()
-    signalandecho = s
-    return signalandecho
+    s1 = sumpf.modules.ShiftSignal(signal=hgm.GetOutput(),shift=shift,circular=False).GetOutput()
+    s1_decay = sumpf.modules.AmplifySignal(input=s1,factor=echo_decay).GetOutput()
+    s = s1_decay + hgm.GetOutput() + speech
+    return s
 
-def nlechocanceller(input,output,init_kernel=None):
-    hgm = nlsp.construct_hgm(kernelfile=nl_system)
-    hgm.SetInput(input)
-    kernel,nlfunc = nlsp.adaptive_identification_powerseries(hgm.GetOutput(),output,branches=1,init_coeffs=init_kernel)
-    iden_hgm = nlsp.HammersteinGroupModel_up(nonlinear_functions=nlfunc,filter_irs=kernel)
-    iden_hgm.SetInput(hgm.GetOutput())
-    return iden_hgm.GetOutput(),kernel
+def nlsystem(branches,excitation,system_identification_alg):
+    ref_hgm = nlsp.construct_hgm(kernelfile=nl_system)
+    ref_hgm.SetInput(excitation.GetOutput())
+    ref_output = ref_hgm.GetOutput()
+    kernel, function = system_identification_alg(excitation,ref_output,branches)
+    iden_hgm = nlsp.HammersteinGroupModel_up(nonlinear_functions=function,filter_irs=kernel,max_harmonics=range(1,branches+1))
+    return iden_hgm,ref_hgm
 
-init_kernel = None
-inputg = [wgn_normal.GetOutput()]
-for input in inputg:
-    echo = generate_nlecho(input)
-    signal = sumpf.modules.SweepGenerator(samplingrate=sampling_rate,length=length).GetSignal()
-    output = echo + signal
-    echoadded,init_kernel = nlechocanceller(input,output,init_kernel)
-    det_signal = output - echoadded
-    # plot.relabelandplot(echo,"echo",show=False)
-    # plot.relabelandplot(signal,"signal",show=False)
-    # plot.relabelandplot(output,"echo+signal",show=False)
-    # plot.relabelandplot(det_signal,"identified",show=True)
-    print nlsp.snr(det_signal,signal)
-    print nlsp.snr(output,signal)
-    print
+def nlechocancellation(input):
+    iden_nl_system, ref_nl_system = nlsystem(branches=branches,excitation=nl_system_iden_excitation,system_identification_alg=system_identification_alg)
+    iden_nl_system.SetInput(input)
+    echoic_op = echoic_nonlinear_chamber(input)
+    desired_op = echoic_op - iden_nl_system.GetOutput()
+    kernel, nlfunction = nlsp.adaptive_identification_legendre(iden_nl_system.GetOutput(),desired_op,branches=1,filtertaps=2**10)
+    iden_lin_system = nlsp.HammersteinGroupModel_up(nonlinear_functions=nlfunction,filter_irs=kernel)
+    iden_lin_system.SetInput(iden_nl_system.GetOutput())
+    determined_output = iden_lin_system.GetOutput() + iden_nl_system.GetOutput()
+    speech = determined_output - desired_op
+    return speech
+
+iden_speech = nlechocancellation(input=wgn_normal.GetOutput())
+nlsp.common.plots.plot(iden_speech,show=False)
+nlsp.common.plots.plot(speech,show=True)
+print nlsp.snr(iden_speech,speech)
+

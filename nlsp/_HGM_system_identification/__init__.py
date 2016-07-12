4a7647810c87862697fac6c41aa37b5c2e9e3d32
Logesh <logeshthamil@gmail.com>
2016-07-12 16:23:20 +0200

    system identification renamed, clipping identification added

--- nlsp/_HGM_system_identification/__init__.py
+++ nlsp/_HGM_system_identification/__init__.py
@@ -1,6 +1,6 @@
-from nonlinear_convolution import *
+from sweep_based import *
 
-from othogonality import *
+from orthogonality_based import *
 
 from linear_identification import linear_identification_temporalreversal,linear_identification,linear_identification_powerhgm
 
